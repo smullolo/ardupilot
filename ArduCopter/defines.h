c1e00684d25345ab7b9bf2742fcea45089e40b41
Isay Konter <isaykonter@gmail.com>
2017-11-06 05:07:18 +0100

    Added flight mode AB

--- ArduCopter/defines.h
+++ ArduCopter/defines.h
@@ -105,6 +105,7 @@ enum control_mode_t {
     AVOID_ADSB =   19,  // automatic avoidance of obstacles in the macro scale - e.g. full-sized aircraft
     GUIDED_NOGPS = 20,  // guided mode but only accepts attitude and altitude
     SMART_RTL =    21,  // SMART_RTL returns to home by retracing its steps
+    AB =       22,  // Isay : automatic zigzag mode.
 };
 
 enum mode_reason_t {
